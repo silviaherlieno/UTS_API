43136f83a90486552ce62d2f148d1ff598dfddb4
silviaherlieno <silviaherlieno00@gmail.com>
2024-03-29 22:07:41 +0700

    UTS API

---
+++ routes/api.php
@@ -0,0 +1,50 @@
+<?php
+
+use App\Http\Controllers\UserController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::post('/users', [UserController::class, 'register']);
+Route::post('/users/login', [UserController::class, 'login']);
+
+Route::middleware(\App\Http\Middleware\ApiAuthMiddleware::class)->group(function () {
+    Route::get('/users/current', [UserController::class, 'get']);
+    Route::patch('/users/current', [UserController::class, 'update']);
+    Route::delete('/users/logout', [UserController::class, 'logout']);
+    Route::post('logout', [UserController::class,'logout']);
+
+    Route::post('/contacts', [\App\Http\Controllers\ContactController::class, 'create']);
+    Route::get('/contacts', [\App\Http\Controllers\ContactController::class, 'search']);
+    Route::get('/contacts/{id}', [\App\Http\Controllers\ContactController::class, 'get'])->where('id', '[0-9]+');
+    Route::put('/contacts/{id}', [\App\Http\Controllers\ContactController::class, 'update'])->where('id', '[0-9]+');
+    Route::delete('/contacts/{id}', [\App\Http\Controllers\ContactController::class, 'delete'])->where('id', '[0-9]+');
+
+    Route::post('/contacts/{idContact}/addresses', [\App\Http\Controllers\AddressController::class, 'create'])
+        ->where('idContact', '[0-9]+');
+    Route::get('/contacts/{idContact}/addresses', [\App\Http\Controllers\AddressController::class, 'list'])
+        ->where('idContact', '[0-9]+');
+    Route::get('/contacts/{idContact}/addresses/{idAddress}', [\App\Http\Controllers\AddressController::class, 'get'])
+        ->where('idContact', '[0-9]+')
+        ->where('idAddress', '[0-9]+');
+    Route::put('/contacts/{idContact}/addresses/{idAddress}', [\App\Http\Controllers\AddressController::class, 'update'])
+        ->where('idContact', '[0-9]+')
+        ->where('idAddress', '[0-9]+');
+    Route::delete('/contacts/{idContact}/addresses/{idAddress}', [\App\Http\Controllers\AddressController::class, 'delete'])
+        ->where('idContact', '[0-9]+')
+        ->where('idAddress', '[0-9]+');
+});
